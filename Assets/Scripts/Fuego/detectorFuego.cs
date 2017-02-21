c02c1aae631abe573f2b61d81c846f74fed037a1
JavierGuillamon <javier.guillamon@live.u-tad.com>
2017-02-21 22:42:08 +0100

    Añadido detector de fuego y apertura de puerta

---
+++ Assets/Scripts/Fuego/detectorFuego.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectorFuego : MonoBehaviour {
+    
+    public GameObject puerta;
+    public Vector3 movimientoPuerta;
+    private bool canOpen = true;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "coffinFire")
+        {
+            if (other.GetComponent<FuegoCoffin>().getOnFire() && canOpen)
+            {
+                StartCoroutine(moveDoor(3f));
+            }
+        }
+    }
+
+    public IEnumerator moveDoor(float time)
+    {
+        canOpen = false;
+        puerta.transform.Translate(movimientoPuerta);
+        yield return new WaitForSeconds(time);
+        Destroy(puerta);
+    }
+}
