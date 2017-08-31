159add89f6252e6f2e0c7a3037a5c177aeed8371
Juan <juanf13579@gmail.com>
2017-08-31 14:28:32 +0200

    Lógica implementada
    A falta del último tramo

---
+++ Assets/Scripts/Post/PlatformSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour {
+
+    public GameObject platformPrefab;
+    public float timeNextSpawn;
+    public float distance;
+
+    [HideInInspector]
+    public Vector3 originalPos;
+    [HideInInspector]
+    public Vector3 finalPos;
+
+    private float currentTime;
+
+    private void Start()
+    {
+        originalPos = transform.position;
+        finalPos = new Vector3(transform.position.x + distance, transform.position.y, transform.position.z);
+        currentTime = timeNextSpawn;
+    }
+
+    private void Update()
+    {
+        currentTime -= Time.deltaTime;
+        if (currentTime <= 0)
+        {
+            Instantiate(platformPrefab, transform.position, Quaternion.identity);
+            currentTime = timeNextSpawn;
+        }
+
+    }
+}
