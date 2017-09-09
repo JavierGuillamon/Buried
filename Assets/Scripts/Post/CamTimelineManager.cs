a90b09e533d3e0449926fe16bf3c2bc83496ae02
Juan <juanf13579@gmail.com>
2017-09-09 18:11:20 +0200

    Scripts de sonido, LUTs y pantalla de título planteada

---
+++ Assets/Scripts/Post/CamTimelineManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CamTimelineManager : MonoBehaviour {
+
+    public PlayableDirector pd;
+    public Animator anim;
+
+    private float timeToChange;
+    private float timeLeft;
+    private bool stop;
+
+
+    void Start () {
+        timeLeft = .1f;
+        pd = GetComponent<PlayableDirector>();
+        stop = false;
+        FindObjectOfType<AudioManager>().Play("AmbientWind");
+    }
+	
+
+	void Update () {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0 && !stop)
+        {
+            pd.Pause();
+        }
+        if (Input.anyKey && !stop)
+        {
+            pd.Play();
+            anim.SetBool("Permiso", true);
+            stop = true;
+            FindObjectOfType<AudioManager>().Play("EntryLevel");
+        }
+    }
+}
